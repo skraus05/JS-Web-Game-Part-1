4a298965203c170f1f448f00ba6ee2bd177f36ac
skraus05 <samuelkraus05@gmail.com>
2023-09-23 01:29:36 -0400

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+//let greenCharacter = document.createElement('img')
+//greenCharacter.src = 'assets/green-character.gif'
+//greenCharacter.style.position = 'fixed'
+//greenCharacter.style.left = '100px'
+//greenCharacter.style.bottom = '100px'
+//document.body.append(greenCharacter)
+
+//let pineTree = document.createElement('img')
+//pineTree.src = 'assets/pine-tree.png'
+//pineTree.style.position = 'fixed'
+//pineTree.style.left = '450px'
+//pineTree.style.bottom = '200px'
+//document.body.append(pineTree)
+
+function newImage(imageURL, left, bottom){
+    let pineTree = document.createElement('img')
+    pineTree.src = imageURL
+    pineTree.style.position = 'fixed'
+    pineTree.style.left = left + 'px'
+    pineTree.style.bottom = bottom + 'px'
+    document.body.append(pineTree)  
+}
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
