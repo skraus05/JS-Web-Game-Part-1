f4e3bb45945ca2fc8e6d7d813a08c081ecce5cf1
Samuel Kraus <samuelkraus@Samuels-MBP.lan>
2023-09-30 12:34:39 -0400

    final commit

--- index.js
+++ index.js
@@ -13,12 +13,13 @@
 //document.body.append(pineTree)
 
 function newImage(imageURL, left, bottom){
-    let pineTree = document.createElement('img')
-    pineTree.src = imageURL
-    pineTree.style.position = 'fixed'
-    pineTree.style.left = left + 'px'
-    pineTree.style.bottom = bottom + 'px'
-    document.body.append(pineTree)  
+    let object = document.createElement('img')
+    object.src = imageURL
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    return object;  
 }
 
 newImage('assets/green-character.gif', 100, 100)
@@ -28,21 +29,21 @@ newImage('assets/pillar.png', 350, 100)
 newImage('assets/crate.png', 150, 200)
 newImage('assets/well.png', 500, 425)
 
-let sword = document.createElement('img')
-sword.src = 'assets/sword.png'
-sword.position = 'fixed'
-sword.left = '500px'
-sword.bottom = '405px'
-document.body.append(sword)
-
-sword.addEventListener('dblclick', function(){
-    sword.remove()
-})
+//let sword = document.createElement('img')
+//sword.src = 'assets/sword.png'
+//sword.position = 'fixed'
+//sword.left = '500px'
+//sword.bottom = '405px'
+//document.body.append(sword)
 
 function newItem(imageURL, left, bottom){
-    sword.style.position = 'fixed';
-    sword.style.left = left + 'px';
-    sword.style.bottom = bottom + 'px';
+    //sword.style.position = 'fixed';
+    //sword.style.left = left + 'px';
+    //sword.style.bottom = bottom + 'px';
+    const obj = newImage(imageURL, left, bottom)
+    obj.addEventListener('dblclick', function(){
+        obj.remove()
+    })
 }
 
 newItem('assets/sword.png' , 500, 405);
